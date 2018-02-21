4f397172e8892bf3f0dd67ddccf804a1bb4e0892
sowdeswaran <36122701+sowdeswaran@users.noreply.github.com>
2018-02-21 14:27:50 +0000

    Create Larger.py

---
+++ Beginner/Larger.py
@@ -0,0 +1,10 @@
+a=float(input("enter the 1st no:"))
+b=float(input("enter the 2nd no:"))
+c=float(input("enter the 3rd no:"))
+if(a>b) and (a>c)
+x=a
+elif(b>a) and (b>c)
+x=b
+print("the large no is:",x)
+else
+print("the large no is:",c)
